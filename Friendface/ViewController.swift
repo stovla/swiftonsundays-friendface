240b51aef3e903bf1c8e608a54d92bd1e38d9255
Vlastimir <vlastimirsd@gmail.com>
2019-01-29 08:08:36 +0100

    starting friend face networking app

---
+++ Friendface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friendface
+//
+//  Created by Vlastimir Radojevic on 1/29/19.
+//  Copyright © 2019 Vlastimir Radojevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
